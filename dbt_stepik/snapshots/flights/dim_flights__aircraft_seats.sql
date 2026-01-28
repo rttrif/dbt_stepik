3650dba1b057f4168d95953173828e9b49ed7a9d
Ruslan_Trifonov <rttrif@rambler.ru>
2026-01-28 21:58:33 +0500

    Исправления после сравнения с эталоном из курса

--- dbt_stepik/snapshots/flights/dim_flights__aircraft_seats.sql
+++ dbt_stepik/snapshots/flights/dim_flights__aircraft_seats.sql
@@ -3,14 +3,21 @@
 {{
    config(
        target_schema='snapshot',
-       unique_key = ['seat_no', 'fare_conditions'],
+       unique_key=['aircraft_code', 'seat_no'],
 
        strategy='check',
-       check_cols=['seat_no', 'fare_conditions'],
+       check_cols=['fare_conditions'],
+       dbt_valid_to_current = "'9999-01-01'::date",
+
+       snapshot_meta_column_names={
+           "dbt_valid_from": "dbt_effective_date_from",
+           "dbt_valid_to": "dbt_effective_date_to"
+       }
    )
 }}
 
 select
+    aircraft_code,
     seat_no,
     fare_conditions
 from {{ ref('stg_flights__seats') }}
--- dbt_stepik/snapshots/flights/dim_flights__aircrafts.sql
+++ dbt_stepik/snapshots/flights/dim_flights__aircrafts.sql
@@ -3,18 +3,23 @@
 {{
    config(
        target_schema='snapshot',
-       unique_key= ['aircraft_code', 'model'],
+       unique_key='aircraft_code',
 
        strategy='check',
-       check_cols=['aircraft_code', 'model', 'seats'],
-       dbt_valid_to_current = "'9999-12-31'::date"
+       check_cols=['model', 'range'],
+       dbt_valid_to_current = "'9999-01-01'::date",
+
+       snapshot_meta_column_names={
+           "dbt_valid_from": "dbt_effective_date_from",
+           "dbt_valid_to": "dbt_effective_date_to"
+       }
    )
 }}
 
 select
     aircraft_code,
     model,
-    range
+    "range"
 from {{ ref('stg_flights__aircrafts') }}
 
 {% endsnapshot %}
